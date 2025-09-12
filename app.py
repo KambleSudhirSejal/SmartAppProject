59dac5619983ff94baeb1cbb1eae69eb9d9ac7ac
KambleSudhirSejal <kamblesejal277@gmail.com>
2025-09-12 15:32:16 +0530

    commit all the changes of changing the port

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def all_devices_hourly():
 
 if __name__ == '__main__':
     database.create_table()
-    app.run(debug=True,use_reloader=False)
+    app.run(host="0.0.0.0",port=8000,debug=True,use_reloader=False)
